691665844ee550ae96f2c28270262043778a4b6f
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2022-04-18 15:01:15 +0700

    route link auth

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const connectDB = require('./config/db')
-// const passport = require('passport')
+const passport = require('passport')
 const bodyParser = require('body-parser')
-// const routes = require('./routes/auth.route')
+const routes = require('./routes/auth.route')
 
 connectDB()
 
@@ -17,9 +17,9 @@ if (process.env.NODE_ENV === 'development') {
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-// app.use(routes)
-// app.use(passport.initialize())
-// require('./config/passport')(passport)
+app.use(routes)
+app.use(passport.initialize())
+require('./config/passport')(passport)
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
