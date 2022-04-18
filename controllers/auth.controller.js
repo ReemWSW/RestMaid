6440c09944754025105955978fc672f6b3d89421
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2022-04-18 15:05:28 +0700

    edit register controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,47 +1,12 @@
 var UserModel = require('../models/User')
 var jwt = require('jwt-simple')
 var config = require('../config/dbconfig')
-const User = require('../models/User')
 
 var functions = {
   registration: function (req, res) {
-    UserModel.findOne({ email: req.body.email }).then((user) => {
-      if (user.email != null) {
-        res.status(300).json({ success: false, message: 'อีเมลล์มีอยู๋แล้ว' })
-      } else {
+    
 
-        if(!req.body.password){
-            res.json({success: false, message:'กรุณากรอกรหัสผ่าน'})
-        }else if(!req.body.name){
-            res.json({success:false, message:'กรุณาใส่ชื่อ'})
-        }else{
-            var newUser = User({
-                email: req.body.email,
-                name: req.body.name,
-                password: req.body.password,
-                phone: req.body.phone,
-            })
-
-            newUser.save(function (err, newUser) {
-                if (err) {
-                  console.log(err)
-                  res
-                    .status(404)
-                    .json({ success: false, message: 'ไม่สามารถลงทะเบลียนได้' })
-                } else {
-                  console.log(newUser)
-                  res.status(200).json({
-                    success: true,
-                    message: 'ลงทะเบียนสำเร็จแล้ว',
-                    data: newUser,
-                  })
-                }
-              })
-        }
-    }
-    })
-
-    var check = User.findOne({ email: req.body.email })
+    var check = UserModel.findOne({ email: req.body.email })
     if (check.email != null) {
       res.status(300).json({ success: false, message: 'อีเมลล์นี้มีอยู่แล้ว' })
     } else {
@@ -50,7 +15,7 @@ var functions = {
       } else if (!req.body.name) {
         res.json({ success: false, message: 'Enter name fields' })
       } else {
-        var newUser = User({
+        var newUser = UserModel({
           email: req.body.email,
           name: req.body.name,
           password: req.body.password,
