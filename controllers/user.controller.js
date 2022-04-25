4c95066f04cfd25ef53620703756c057fce7df28
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2022-04-26 04:38:47 +0700

    setmaid user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,27 +1,27 @@
 UserModel = require('../models/User')
 
 var functions = {
-    setMaid: function (req, res) {
-        if (!req.body) {
-          res.json({ success: false, message: 'เกิดข้อผิดพลาด' })
+  setMaid: function (req, res) {
+    if (!req.body) {
+      res.json({ success: false, message: 'เกิดข้อผิดพลาด' })
+    } else {
+      var id = req.body.id
+      UserModel.updateOne({ _id: id }, { $set: { maid: true } }, function (
+        err,
+        user,
+      ) {
+        if (err) {
+          res.status(404).json({ success: false, message: 'เกิดข้อผิดพลาด' })
         } else {
-          var id = req.body.id
-          UserModel.updateOne({ _id: id }, { $set: { maid: true } }, function (
-            err,
-            user,
-          ) {
-            if (err) {
-              res.status(404).json({ success: false, message: 'เกิดข้อผิดพลาด' })
-            } else {
-              res.status(200).json({
-                success: true,
-                message: 'อัพเดทสำเร็จแล้ว',
-                data: user,
-              })
-            }
+          console.log(user)
+          res.status(200).json({
+            success: true,
+            message: 'อัพเดทสำเร็จแล้ว',
           })
         }
-      },
+      })
+    }
+  },
 }
 
 module.exports = functions
